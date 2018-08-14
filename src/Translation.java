49b918c88052972ef9a44b3efff254057e2291ec
_c0da_ <duvictor514@gmail.com>
2018-08-14 12:11:04 -0700

    Removed newline carriages until support is added

--- src/Translation.java
+++ src/Translation.java
@@ -5,17 +5,17 @@ public class Translation
 	public static void main (String[] args) throws IOException
 	{
 		Translator t = new Translator("definitions.json", false);
-		System.out.println(t.getTranslated("I'm like bitch who is yo mans?\n"
-				+ "Can't keep my dick in my pants!\n"
-				+ "My bitch don't love me no more, aye\n"
-				+ "She kick me out I'm like vro, aye\n"
-				+ "That bitch don't wanna be friends, aye\n"
-				+ "I gave hre dick, she got mad, aye\n"
-				+ "She put her tongue on my dick, aye\n"
-				+ "Look at my wrist, about 10, aye\n"
-				+ "Just got a pound of that boof, aye\n"
-				+ "Brought that shit straight to the boof, aye\n"
-				+ "Tommy my Hilfiger boots, aye\n"
-				+ "She said want fuck bitch, I do, aye\n"));
+		System.out.println(t.getTranslated("I'm like bitch who is yo mans? "
+				+ "Can't keep my dick in my pants! "
+				+ "My bitch don't love me no more, aye "
+				+ "She kick me out I'm like vro, aye "
+				+ "That bitch don't wanna be friends, aye "
+				+ "I gave hre dick, she got mad, aye "
+				+ "She put her tongue on my dick, aye "
+				+ "Look at my wrist, about 10, aye "
+				+ "Just got a pound of that boof, aye "
+				+ "Brought that shit straight to the boof, aye "
+				+ "Tommy my Hilfiger boots, aye "
+				+ "She said want fuck bitch, I do, aye"));
 	}
 }
