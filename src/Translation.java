c0c6d7fcdfb19fedba7639d740a76eb14e3f6cbd
_c0da_ <duvictor514@gmail.com>
2018-09-07 16:08:15 -0700

    Make translator example interactive

--- src/Translation.java
+++ src/Translation.java
@@ -1,21 +1,17 @@
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Translation
 {
 	public static void main (String[] args) throws IOException
 	{
 		Translator t = new Translator("definitions.json", false);
-		System.out.println(t.getTranslated("I'm like bitch who is yo mans? "
-				+ "Can't keep my dick in my pants! "
-				+ "My bitch don't love me no more, aye "
-				+ "She kick me out I'm like vro, aye "
-				+ "That bitch don't wanna be friends, aye "
-				+ "I gave hre dick, she got mad, aye "
-				+ "She put her tongue on my dick, aye "
-				+ "Look at my wrist, about 10, aye "
-				+ "Just got a pound of that boof, aye "
-				+ "Brought that shit straight to the boof, aye "
-				+ "Tommy my Hilfiger boots, aye "
-				+ "She said want fuck bitch, I do, aye"));
+		Scanner sc = new Scanner(System.in);
+		for(;;)
+		{
+			System.out.println("Please enter English sentence: ");
+			String s = sc.nextLine();
+			System.out.println(t.getTranslated(s));
+		}
 	}
 }
